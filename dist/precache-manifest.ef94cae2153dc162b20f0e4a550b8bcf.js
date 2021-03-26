55aca721fa3975d5d58e583eb26b1f45a41fdb08
Galba Arueira <arueira95@gmail.com>
2021-03-26 20:18:24 -0300

    update dist

---
+++ dist/precache-manifest.ef94cae2153dc162b20f0e4a550b8bcf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2959c7e439390df24def",
+    "url": "chunk-vendors.f29b41fe.css"
+  },
+  {
+    "revision": "2959c7e439390df24def",
+    "url": "chunk-vendors.2959c7e4.js"
+  },
+  {
+    "revision": "51c34bbbb30b0bef4cd1",
+    "url": "app.6f102eb0.css"
+  },
+  {
+    "revision": "51c34bbbb30b0bef4cd1",
+    "url": "app.51c34bbb.js"
+  },
+  {
+    "revision": "5a4618f029618cc2795c05fe53d57028",
+    "url": "fonts/fa-regular-400.5a4618f0.eot"
+  },
+  {
+    "revision": "00ddaede094b83270cadbfc1a907e8ca",
+    "url": "fonts/fa-solid-900.00ddaede.ttf"
+  },
+  {
+    "revision": "666a82cb3e9f8591bef4049aea26c4c6",
+    "url": "img/fa-solid-900.666a82cb.svg"
+  },
+  {
+    "revision": "18d2347ab2a9f40ca2247cdb03303d84",
+    "url": "fonts/fa-solid-900.18d2347a.woff2"
+  },
+  {
+    "revision": "e9d8dbb0c3e9b97ffc59c50ff5d01422",
+    "url": "img/fa-regular-400.e9d8dbb0.svg"
+  },
+  {
+    "revision": "e99569d3d10c94c60d9a68523c1c0e71",
+    "url": "fonts/fa-regular-400.e99569d3.woff"
+  },
+  {
+    "revision": "6534c603e0892488132ad57248ec69e2",
+    "url": "fonts/fa-regular-400.6534c603.ttf"
+  },
+  {
+    "revision": "128d2b0be23925e5cf36717ddc6f093b",
+    "url": "fonts/fa-solid-900.128d2b0b.woff"
+  },
+  {
+    "revision": "a3715c6fe264a51f1d9260b447ff46bc",
+    "url": "fonts/fa-regular-400.a3715c6f.woff2"
+  },
+  {
+    "revision": "d9824d00712532d7697df68df16ae0d3",
+    "url": "fonts/fa-solid-900.d9824d00.eot"
+  },
+  {
+    "revision": "921cddaa07f7805ec43b739504fb0a1f",
+    "url": "img/logo.921cddaa.png"
+  },
+  {
+    "revision": "aa626844f898be87079bc7e306fd747b",
+    "url": "index.html"
+  }
+];
